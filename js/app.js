a44a3c5f16cf4492d4ee95c007f0a3080fef3a43
Paul Harriman <dovernh@gmail.com>
2017-07-23 17:43:13 -0400

    sticking header, no communication

--- js/app.js
+++ js/app.js
@@ -1,27 +1,39 @@
-(function () {
+(function (options) {
     'use strict';
 
     iFrameResize({
         checkOrigin: false
     });
 
-    setTimeout(function(){
-        window.addEventListener('scroll', function() {
-            var iframe = document.getElementById('myIframe'),
-                innerDoc = iframe.contentDocument || iframe.contentWindow.document,
-                header = innerDoc.getElementsByClassName('header'),
-                leftNav = innerDoc.getElementsByClassName('.left-nav'),
-                hdrHgt = header.clientHeight;
-            
-            window.addEventListener('scroll', function(){
-                var scroll = window.pageYOffset || 
-                             document.documentElement.scrollTop || 
-                             document.body.scrollTop ||
-                             0;
+    function scrollIframe (scroll) {
+        var iframe = document.querySelector(options.iframeSel),
+            innerDoc = iframe.contentDocument || iframe.contentWindow.document,
+            header = document.querySelector(options.iframeHdr),
+            leftNav = document.querySelector(options.iframeLeftNav),
+            hdrHgt = header.clientHeight;
 
-                //header.style.top = + 'px';
-                //leftNav.style.top = + 'px'
-            })
-        });
-    }, 500);
-}())
+        //header.style.top = + 'px';
+        leftNav.style.top = scroll + 'px'
+    }
+
+    function iframeReady () {
+        window.addEventListener('scroll', function() {
+            var scroll = window.pageYOffset || 
+                            document.documentElement.scrollTop || 
+                            document.body.scrollTop ||
+                            0;
+
+            console.log('scroll', scroll);
+
+            scrollIframe(scroll);
+        });     
+    }
+
+    setTimeout(iframeReady, 500);
+}(
+    {
+        iframeSel: '#myIframe',
+        iframeHdr: '.iframe .header',
+        iframeLeftNav: '.iframe .left-nav'
+    }
+));
